26689d442844e1c623ebb721358d86110634fe1b
Abdulla Vennakode <abdullacp9048@gmail.com>
2025-10-01 19:49:49 +0530

    Committed after completion of for in loop & for of loop

---
+++ JavascriptNew/indexFive.js
@@ -0,0 +1,25 @@
+// //for in loop
+// let objectOne = {
+//     name:"Abdulla",
+//     place:"Vennakode",
+//     email:"abdullacp9048@gmail.com",
+//     number:"9048688824",
+//     bloodGroup:"A+",
+//     Qualification:"Graduation",
+//     Skill:"Designing"
+// }
+
+
+// for(let i in objectOne){
+//     console.log(`${i}: ${objectOne[i]}`);
+    
+// }
+
+
+// //for of loop
+// let arr = [1,2,3,4,5,6,7];
+
+// for(let z of arr){
+//     console.log(z);
+    
+// }
